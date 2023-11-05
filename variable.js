87741b25992b2351f93fe374e49af4d9381124cc
Rishabh Singh Satya <80286651+rishabhsinghsatya@users.noreply.github.com>
2023-11-05 11:27:16 +0530

    Delete variable.js

--- variable.js
+++
@@ -1,7 +0,0 @@
-const myNumber = 3; // a number
-const myString = "Hello, World!"; // a string
-const myBoolean = true; // a boolean
-
-console.log(myNumber);
-console.log(myString);
-console.log(myBoolean);
