8a8978c491c1a165cf6e29cdb3e6110f594ddac4
Rishabh Singh <vineetsatya36@gmail.com>
2023-11-06 11:16:06 +0530

    comparisons in js

---
+++ 5-comparisons.js
@@ -0,0 +1,33 @@
+//Equality (==) and Inequality (!=):
+let a = 5;
+let b = 10;
+console.log(a == b); // false
+console.log(a != b); // true
+
+//Strict Equality (===) and Strict Inequality (!==):
+let x = 5;
+let y = "5";
+console.log(x === y); // false
+console.log(x !== y); // true
+
+// Greater than (>) and Less than (<):
+var p = 15;
+var q = 10;
+console.log(p > q); // true (p is greater than q)
+console.log(p < q); // false (p is not less than q)
+
+// Greater than or equal to (>=) and Less than or equal to (<=):
+var m = 20;
+var n = 20;
+console.log(m >= n); // true (m is greater than or equal to n)
+console.log(m <= n); // true (m is less than or equal to n)
+
+// Logical AND (&&) and Logical OR (||):
+var isSunny = true;
+var isWarm = true;
+console.log(isSunny && isWarm); // true (both conditions are true)
+console.log(isSunny || isWarm); // true (at least one condition is true)
+
+// Negation (!):
+var isRainy = false;
+console.log(!isRainy); // true (negates false to true)
