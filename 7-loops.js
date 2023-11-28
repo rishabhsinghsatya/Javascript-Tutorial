8481c5c56d0c162fd500a63353eb665fe7ad96a9
Rishabh Singh <vineetsatya36@gmail.com>
2023-11-28 12:54:02 +0530

    loops in js

---
+++ 7-loops.js
@@ -0,0 +1,19 @@
+//1. for Loop:
+// Example: Print numbers 1 to 5 using a for loop, while loop, doWhile loop
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
+
+//   2. while Loop:
+let counter = 1;
+while (counter <= 5) {
+  console.log(counter);
+  counter++;
+}
+
+// 3. do...while Loop:
+let num = 1;
+do {
+  console.log(num);
+  num++;
+} while (num <= 5);
