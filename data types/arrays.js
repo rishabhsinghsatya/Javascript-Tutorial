f9492652147ee2491a349cbe074d271a45fdd7dd
Rishabh Singh <vineetsatya36@gmail.com>
2024-05-31 19:03:23 +0530

    reduce function

---
+++ data types/arrays.js
@@ -0,0 +1,36 @@
+const myArr = [0, 1, 2, 3, 4, 5];
+console.log(myArr);
+
+// const newArr = new Array(1, 2, 3, 5, 8);
+// console.log(newArr[1]);
+
+//array methods ==============================================================
+// myArr.push(6);
+// console.log(myArr);
+
+// myArr.push(7);
+// console.log(myArr);
+
+// myArr.pop();
+// console.log(myArr);
+
+// myArr.unshift(9);
+// console.log(myArr);
+// myArr.shift();
+// console.log(myArr);
+
+// console.log(myArr.includes(9)); //false
+// console.log(myArr.indexOf(9)); //-1
+// console.log(myArr.indexOf(5)); //5
+
+// const newArr = myArr.join();
+
+// console.log(typeof newArr);
+// console.log(newArr);
+// console.log(myArr);
+
+//slice or splice ===========================================================
+
+//spread operator=====================
+
+//flat function=======================
