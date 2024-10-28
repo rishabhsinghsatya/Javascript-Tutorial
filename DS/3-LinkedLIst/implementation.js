c97ca3b614b1916c4f591b35c6057e41d688d84f
Rishabh Singh <vineetsatya36@gmail.com>
2024-10-29 00:26:28 +0530

    linked list implementation

--- DS/3-LinkedLIst/implementation.js
+++ DS/3-LinkedLIst/implementation.js
@@ -10,9 +10,112 @@ class LinkedList {
     this.head = null;
   }
 
-  addFirst(data){
+  addFirst(data) {
     const newNode = new Node(data);
     newNode.next = this.head;
     this.head = newNode;
   }
+
+  addLast(data) {
+    const newNode = new Node(data);
+
+    if (!this.head) {
+      this.head = newNode;
+      return;
+    }
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+    current.next = newNode;
+  }
+
+  size() {
+    let count = 0;
+    let current = this.head;
+    while (current) {
+      count++;
+      current = current.next;
+    }
+    return count;
+  }
+
+  addAt(index, data) {
+    if (index < 0 || index > this.size()) {
+      console.log("Invalid Index");
+      return;
+    }
+
+    const newNode = new Node(data);
+    if (index === 0) {
+      newNode.next = this.head;
+      this.head = newNode;
+      return;
+    }
+
+    let current = this.head;
+    for (let i = 0; i < index - 1; i++) {
+      current = current.next;
+    }
+    newNode.next = current.next;
+    current.next = newNode;
+  }
+
+  removeTop() {
+    if (!this.head) {
+      return;
+    }
+
+    this.head = this.head.next;
+  }
+
+  removeLast() {
+    if (!this.head) {
+      return;
+    }
+
+    let current = this.head;
+    while (current.next.next) {
+      current = current.next;
+    }
+    current.next = null;
+  }
+
+  removeAt(index) {
+    if (index < 0 || index > this.size()) {
+      console.log("Invalid Index");
+      return;
+    }
+
+    const newNode = new Node(data);
+    if (index === 0) {
+      newNode.next = this.head;
+      this.head = newNode;
+      return;
+    }
+
+    let current = this.head;
+    for (let i = 0; i < index - 1; i++) {
+      current = current.next;
+    }
+    newNode.next = current.next;
+    current.next = newNode;
+  }
+
+  print() {
+    let current = this.head;
+    while (current) {
+      console.log(current.data);
+      current = current.next;
+    }
+  }
 }
+
+const linkedList = new LinkedList();
+linkedList.addFirst(5);
+linkedList.addFirst(3);
+linkedList.addFirst(8);
+
+linkedList.addLast(6);
+
+linkedList.print();
